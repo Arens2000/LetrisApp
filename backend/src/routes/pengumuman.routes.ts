eb20ec8d6eada5a398966488c753b542224eaa15
Arens Gulo <136255669+Arens2000@users.noreply.github.com>
2025-12-06 03:16:15 +0700

    Update pengumuman.routes.ts

--- backend/src/routes/pengumuman.routes.ts
+++ backend/src/routes/pengumuman.routes.ts
@@ -1,1 +1,33 @@
+import { Router } from "express";
+import { PengumumanController } from "../controllers/PengumumanController";
+import { verifyToken } from "../middleware/auth";
+import { requireRole } from "../middleware/role";
+
+const router = Router();
+
+// Buat pengumuman
+router.post(
+  "/",
+  verifyToken,
+  requireRole(["guru", "admin"]),
+  PengumumanController.create
+);
+
+// Semua pengumuman
+router.get(
+  "/",
+  verifyToken,
+  requireRole(["siswa", "guru", "admin"]),
+  PengumumanController.list
+);
+
+// Hapus
+router.delete(
+  "/:id",
+  verifyToken,
+  requireRole(["admin"]),
+  PengumumanController.delete
+);
+
+export default router;
 
