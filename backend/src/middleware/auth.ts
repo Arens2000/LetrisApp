152f86baa40ff87833c7205156e7f7926f6e9c0b
Arens Gulo <136255669+Arens2000@users.noreply.github.com>
2025-12-06 02:50:27 +0700

    Update auth.ts

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,1 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { authAdmin } from "../config/firebase";
+
+export async function verifyToken(req: any, res: Response, next: NextFunction) {
+  const header = req.headers.authorization;
+
+  if (!header) return res.status(401).json({ message: "Unauthorized" });
+
+  const token = header.split(" ")[1];
+
+  try {
+    const decoded = await authAdmin.verifyIdToken(token);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: "Invalid Token" });
+  }
+}
 
