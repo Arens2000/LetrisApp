2f2c22d7b19b2486ff4b0960d1c0824b5450f61a
Arens Gulo <136255669+Arens2000@users.noreply.github.com>
2025-12-06 02:51:34 +0700

    Update UserController.ts

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,1 +1,21 @@
+import { db } from "../config/firebase";
+
+export class UserController {
+  static async getAll(req: any, res: any) {
+    const snap = await db.collection("users").get();
+    res.json(snap.docs.map((d) => d.data()));
+  }
+
+  static async updateUser(req: any, res: any) {
+    const { uid } = req.params;
+    await db.collection("users").doc(uid).update(req.body);
+    res.json({ message: "User updated" });
+  }
+
+  static async deleteUser(req: any, res: any) {
+    const { uid } = req.params;
+    await db.collection("users").doc(uid).delete();
+    res.json({ message: "User deleted" });
+  }
+}
 
