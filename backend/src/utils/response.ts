612171805bc69128fcfa2d042b5b0ee5cc0ac6e1
Arens Gulo <136255669+Arens2000@users.noreply.github.com>
2025-12-06 03:07:46 +0700

    Update response.ts

--- backend/src/utils/response.ts
+++ backend/src/utils/response.ts
@@ -1,1 +1,15 @@
+export function success(res: any, message: string, data: any = null) {
+  return res.json({
+    status: "success",
+    message,
+    data,
+  });
+}
+
+export function error(res: any, message: string, code: number = 400) {
+  return res.status(code).json({
+    status: "error",
+    message,
+  });
+}
 
