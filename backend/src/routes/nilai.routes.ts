6e06523a776cde341ab88fbafb95f8ada1a4a4ee
Arens Gulo <136255669+Arens2000@users.noreply.github.com>
2025-12-06 02:54:03 +0700

    Update nilai.routes.ts

--- backend/src/routes/nilai.routes.ts
+++ backend/src/routes/nilai.routes.ts
@@ -1,1 +1,12 @@
+import { Router } from "express";
+import { NilaiController } from "../controllers/NilaiController";
+import { verifyToken } from "../middleware/auth";
+
+const router = Router();
+
+router.post("/", verifyToken, NilaiController.addNilai);
+router.get("/kelas/:kelas", verifyToken, NilaiController.getNilaiKelas);
+router.get("/siswa/:uid", verifyToken, NilaiController.getNilaiSiswa);
+
+export default router;
 
