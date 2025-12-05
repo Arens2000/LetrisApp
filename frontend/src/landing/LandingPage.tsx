a574811042d72597d1dc8e894b2b8c038b404483
Arens Gulo <136255669+Arens2000@users.noreply.github.com>
2025-12-06 01:08:54 +0700

    Create LandingPage.tsx

---
+++ frontend/src/landing/LandingPage.tsx
@@ -0,0 +1,19 @@
+import HeroSection from "./HeroSection";
+import FiturSection from "./FiturSection";
+import ProgramKeahlian from "./ProgramKeahlian";
+import VisiMisi from "./VisiMisi";
+import TestimoniSection from "./TestimoniSection";
+import Footer from "./Footer";
+
+export default function LandingPage() {
+  return (
+    <>
+      <HeroSection />
+      <FiturSection />
+      <ProgramKeahlian />
+      <VisiMisi />
+      <TestimoniSection />
+      <Footer />
+    </>
+  );
+}
